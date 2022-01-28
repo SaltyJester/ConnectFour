b07ac30e2e7f1893befaef65759b32c0f47d7c89
Young Choe <01youngdc@gmail.com>
2022-01-27 22:22:23 -0800

    got click handler to work

--- public/script.js
+++ public/script.js
@@ -1,14 +1,17 @@
 /*
 Board Logic, follows [row][col] format
 0 is empty, 1 is red, 2 is yellow
+"drop[x]" is the next available spot in x column on the board
 */
 let board = [];
+let drop = [];
+let curPlayer = 1;
 for(let i = 0; i < 6; i++){
     board.push(new Array(7).fill(0));
 }
-
-board[2][2] = 1; // test
-board[2][5] = 2; // test
+for(let i = 0; i < 7; i++){
+    drop.push(0);
+}
 
 /*
 Canvas Logic
@@ -47,5 +50,21 @@ function draw(){
 }
 
 function clickHandler(event){
-    console.log('Hello!');
+    let rect = event.target.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top; // we don't really need this info
+    let col = Math.floor(x/100);
+
+    if(drop[col] < 6){
+        board[5 - drop[col]][col] = curPlayer;
+        drop[col]++;
+        curPlayer = 3 - curPlayer;
+
+        // for testing, delete later
+        console.log(board);
+        console.log(drop);
+        console.log('col: ' + col);
+    }
+
+    draw();
 }
