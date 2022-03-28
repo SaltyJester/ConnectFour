8ed7f8f103a031a533e08531ddcd8a3e97cc33a6
Young Choe <01youngdc@gmail.com>
2022-03-27 17:45:00 -0700

    able to generate shareable game from landing page, somewhat curde looking atm

--- public/landing_script.js
+++ public/landing_script.js
@@ -1,3 +1,9 @@
-function createGame(){
+let url = window.location.href;
 
+async function createGame(){
+    let response = await fetch('create_game', {
+        method: 'POST'
+    });
+    let data = await response.json();
+    document.getElementById('link_share').innerHTML = url + 'join/' + data.sessionID;
 }
